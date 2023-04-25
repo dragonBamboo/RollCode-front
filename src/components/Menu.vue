33e720a28edc68fc4947fd00d0182b4a9ddbfbcb
dragon bamboo <yt000000x@163.com>
2023-04-25 20:13:00 +0800

    1

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -5,9 +5,11 @@
             mode="horizontal"
             @select="handleSelect"
     >
-        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-menu-item index="1">
+            首页
+        </el-menu-item>
         <el-sub-menu index="2">
-            <template #title>Workspace</template>
+            <template #title>题库</template>
             <el-menu-item index="2-1">item one</el-menu-item>
             <el-menu-item index="2-2">item two</el-menu-item>
             <el-menu-item index="2-3">item three</el-menu-item>
@@ -18,43 +20,37 @@
                 <el-menu-item index="2-4-3">item three</el-menu-item>
             </el-sub-menu>
         </el-sub-menu>
-        <el-menu-item index="3" disabled>Info</el-menu-item>
-        <el-menu-item index="4">Orders</el-menu-item>
-    </el-menu>
-    <div class="h-6" />
-    <el-menu
-            :default-active="activeIndex2"
-            class="el-menu-demo"
-            mode="horizontal"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b"
-            @select="handleSelect"
-    >
-        <el-menu-item index="1">Processing Center</el-menu-item>
-        <el-sub-menu index="2">
-            <template #title>Workspace</template>
-            <el-menu-item index="2-1">item one</el-menu-item>
-            <el-menu-item index="2-2">item two</el-menu-item>
-            <el-menu-item index="2-3">item three</el-menu-item>
-            <el-sub-menu index="2-4">
-                <template #title>item four</template>
-                <el-menu-item index="2-4-1">item one</el-menu-item>
-                <el-menu-item index="2-4-2">item two</el-menu-item>
-                <el-menu-item index="2-4-3">item three</el-menu-item>
-            </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="3" disabled>Info</el-menu-item>
-        <el-menu-item index="4">Orders</el-menu-item>
+        <el-menu-item index="3" >面试disabled</el-menu-item>
+        <el-menu-item index="4">求职</el-menu-item>
+        <el-menu-item index="5">学习</el-menu-item>
+        <el-menu-item index="6">竞赛</el-menu-item>
     </el-menu>
 </template>
 
 <script>
 export default {
-    name: "Menu"
+    name: "Menu",
+    data() {
+        return {
+            activeIndex: '1'
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key,keyPath)
+        },
+    }
 }
 </script>
 
 <style scoped>
+  #index-img{
+      height: 56px;
+  }
+  .el-menu-demo/deep/{
+      height: 56px;
+      width: auto;
+  }
+
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <el-container>
+            <el-header>
+                <el-row :gutter="20">
+                    <el-col :span="1"><div class="grid-content ep-bg-purple" /></el-col>
+                    <el-col :span="1"><img src="../assets/logo.png" id="index-img" alt="logo"></el-col>
+                    <el-col :span="11">
+                        <Menu></Menu>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-input v-model="input" placeholder="Please input" />
+                    </el-col>
+                    <el-col :span="2"><div class="grid-content ep-bg-purple" /></el-col>
+                    <el-col :span="1"><div class="grid-content ep-bg-purple" /></el-col>
+                </el-row>
+
+            </el-header>
+            <el-main>Main</el-main>
+        </el-container>
+    </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+// import {Options, Vue} from 'vue-class-component';
+// import HelloWorld from '@/components/HelloWorld.vue';
+// import Menu from "@/components/Menu.vue"; // @ is an alias to /src
+//
+// @Options({
+//     components: {
+//         Menu,
+//         // HelloWorld,
+//     },
+// })
+// export default class HomeView extends Vue {
+// }
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+import Menu from "@/components/Menu.vue";
+
+export default {
+    name: 'home',
+    components:{
+        Menu
+    },
+    data(){
+        return{
+            input:''
+        }
+    }
+}
+
 </script>
+<style lang="less" scoped>
+    #index-img{
+        height: 56px;
+    }
+    .el-input/deep/{
+        height: 56px;
+    }
+</style>
