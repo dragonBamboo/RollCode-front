eba10b9f98c0401cefdd1127857008aac1eca186
dragonBamboo <YT000000X@163.com>
2023-04-26 08:51:50 +0800

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,16 +6,26 @@
   
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import IHome from '@/components/IHome.vue'
-@Options({
-  components: {
-    IHome,
-    HelloWorld
-  },
-})
-export default class HomeView extends Vue {}
+<!--<script lang="ts">-->
+<!--import { Options, Vue } from 'vue-class-component';-->
+<!--import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src-->
+<!--import IHome from '@/components/IHome.vue'-->
+<!--@Options({-->
+<!--  components: {-->
+<!--    IHome,-->
+<!--    HelloWorld-->
+<!--  },-->
+<!--})-->
+<!--export default class HomeView extends Vue {}-->
 
+<!--</script>-->
+<script>
+import IHome from "@/components/IHome.vue";
+
+export default {
+    name:'home',
+    components:{
+        IHome,
+    }
+}
 </script>
